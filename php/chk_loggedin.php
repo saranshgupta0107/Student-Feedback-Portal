619d1d5f24c71986ebbf72406d04644a85378e84
Kuber144 <kuberjain144@gmail.com>
2023-02-14 13:08:50 +0530

    chkloggedin

--- php/chk_loggedin.php
+++ php/chk_loggedin.php
@@ -1,29 +1,30 @@
 <?php
 session_start();
 ?>
-<!DOCTYPE html>
-<html>
-    <body>
         <?php 
         $curPageName=substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+        $userid=$_SESSION['userid'];
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']== true)
 {
     if(isset($_SESSION['userid']))
     {
-        if($_SESSION['userid']==1 && ($curPageName=="admin.html" || $curPageName="add_faculty.html" || $curPageName="add_faculty_course.html" || $curPageName="add_student.html" || $curPageName="show_faculty.html" || $curPageName="view_feedback.html"))
+        if($_SESSION['userid']==1 && ($curPageName=="admin.html" || $curPageName=="add_faculty.html" || $curPageName=="add_faculty_course.html" || $curPageName=="add_student.html" || $curPageName=="show_faculty.html" || $curPageName=="view_feedback.html" || $curPageName=="view_student.html"))
+        {
+        }
+        elseif($_SESSION['userid']==2 && ($curPageName=="faculty.html" || $curPageName=="show_feedback.html"))
+        {
+        }
+        elseif($_SESSION['userid']==3 && ($curPageName=="student.html" || $curPageName=="give_feedback.html" || $curPageName=="view_feedback.html"))
         {}
-        elseif($_SESSION['userid']==2 && $curPageName="faculty.html")
-        {}
-        elseif($_SESSION['userid']==3 && $curPageName="student.html")
-        {}
-        else
-        header("location: ../../index.html");
+        else{
+        echo "<script>window.location.replace('../../index.html');</script>";
+        header("location: ../../index.html");}
     }
-    else
-    header("location: ../../index.html");
+    else{
+    echo '<script>window.location.replace("../../index.html");</script>';
+    header("location: ../../index.html");}
 }
-else
-header("location: ../../index.html");
+else{
+echo '<script>window.location.replace("../../index.html");</script>';
+header("location: ../../index.html");}
 ?>  
-</body>
-</html>
