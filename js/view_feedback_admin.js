74f9866d0eb70c7739c1155b8dda6cdd345ad638
jaimoryani <jaimoryanir@gmail.com>
2023-04-05 00:07:40 +0530

    changes

--- js/view_feedback_admin.js
+++ js/view_feedback_admin.js
@@ -23,12 +23,12 @@ function addLis(i) {
 function show_data() {
     document.getElementById('top').innerHTML = '';
     var str = '';
-    str += "<table class='tablefeed' width='100%'>";
+    str += "<table class='table'>";
     var columns = Object.keys(data[0]);
-    str += "<thead>";
+    str += "<thead class='p-3 mb-2 bg-primary text-white'>";
     str += "<tr>";
     for (var i = 0; i < columns.length; i++) {
-        str += ("<th>" + columns[i] + "</th>");
+        str += ("<th scope='col'>" + columns[i].toUpperCase + "</th>");
     }
     str += ("<th>" + "</th>")
     str += "</tr>";
@@ -39,7 +39,7 @@ function show_data() {
         for (var j = 0; j < columns.length; j++) {
             str += ("<td>" + data[i][columns[j]] + "</td>");
         }
-        str += (`<td><button id=${'btn' + i}>delete</button></td>`);
+        str += (`<td><button id=${'btn' + i} class='btn btn-primary'>delete</button></td>`);
         str += "</tr>";
     }
     str += "</tbody>";
